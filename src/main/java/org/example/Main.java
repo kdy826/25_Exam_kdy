d4870e36ff1830d5f90a2427af3ac6c641570bce
kdy826 <kimdy66695@gmail.com>
2025-03-20 16:23:48 +0900

    Examkdy2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,22 +7,18 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-
-        for(int i = 0; i< b; i++){
-            for(int j = 0; j< a; j++){
-                System.out.printf("*");
-
-            }
-            System.out.printf("\n");
-            System.out.println();
-
-
+        int n = sc.nextInt();
+        int sum = n;
+        int j = 1;
+        for (int i = 0; i < n; i++) {
+            System.out.printf("%d ", sum);
+            sum = n - j;
+            ++j;
         }
-
-
-
-
     }
 }
+
+
+//continue n = n - 1 ??
+
+
